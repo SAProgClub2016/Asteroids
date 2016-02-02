f0c65ad21362cfb0ee118279b9355792f11e9b26
AJiangSewickley <aj100102@gmail.com>
2016-02-02 12:31:01 -0500

    Draw

--- Asteroids/Asteroids/Game.cs
+++ Asteroids/Asteroids/Game.cs
@@ -12,21 +12,36 @@ namespace Asteroids
 {
     public partial class Game : Form
     {
+        Player player;
+        System.Drawing.Graphics graphics;
         public Game()
         {
             InitializeComponent();
             FormBorderStyle = FormBorderStyle.None;
             WindowState = FormWindowState.Maximized;
-            Player player = new Player(GameScreen.Size.Width / 2, GameScreen.Size.Height / 2);
+            player = new Player(GameScreen.Size.Width / 2, GameScreen.Size.Height / 2);
+            graphics = this.CreateGraphics();
+            GameScreen.Paint += new PaintEventHandler(GameScreen_Paint);
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-
+            //System.Drawing.Rectangle rectangle = new System.Drawing.Rectangle(player.getX()-50, player.getY()-50,player.getX()+50,player.getY()+50);
+            //graphics.DrawEllipse(System.Drawing.Pens.Black, rectangle);
         }
 
         private void GameScreen_Paint(object sender, PaintEventArgs e)
         {
+            var p = sender as Panel;
+            var g = e.Graphics;
+            g.FillRectangle(new SolidBrush(Color.FromArgb(0, Color.White)), p.DisplayRectangle);
+            Point[] points = new Point[4];
+            points[0] = new Point(player.getX() - 50, player.getY() - 50);
+            points[1] = new Point(player.getX() - 50, player.getY() + 50);
+            points[2] = new Point(player.getX() + 50, player.getY() + 50);
+            points[3] = new Point(player.getX() + 50, player.getY() - 50);
+            Brush brush = new SolidBrush(Color.DarkGreen);
+            g.FillPolygon(brush, points);
 
         }
     }

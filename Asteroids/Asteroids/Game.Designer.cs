52c267d31f125926a4b6b4da0ba09aa75dc1c43f
AJiangSewickley <aj100102@gmail.com>
2016-02-23 16:33:27 -0500

    Undo

--- Asteroids/Asteroids/Game.Designer.cs
+++ Asteroids/Asteroids/Game.Designer.cs
@@ -28,25 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.GameScreen = new System.Windows.Forms.Panel();
             this.SuspendLayout();
-            // 
-            // timer1
-            // 
-            this.timer1.Enabled = true;
-            this.timer1.Interval = 10;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
-            // GameScreen
-            // 
-            this.GameScreen.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.GameScreen.Location = new System.Drawing.Point(0, 0);
-            this.GameScreen.Name = "GameScreen";
-            this.GameScreen.Size = new System.Drawing.Size(1002, 519);
-            this.GameScreen.TabIndex = 0;
-            this.GameScreen.Paint += new System.Windows.Forms.PaintEventHandler(this.GameScreen_Paint);
             // 
             // Game
             // 
@@ -54,7 +36,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(1002, 519);
-            this.Controls.Add(this.GameScreen);
             this.Name = "Game";
             this.Text = "Asteroids";
             this.ResumeLayout(false);
@@ -62,9 +43,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Panel GameScreen;
     }
 }
 
